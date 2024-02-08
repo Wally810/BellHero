2f5b07b27d44cf485119a47eaaf3bb8ec65b6798
Charley Dodgion <charleydodgion@suumail.net>
2024-02-08 15:37:54 -0700

    Weh

--- Assets/Scripts/SoundTransfrom.cs
+++ Assets/Scripts/SoundTransfrom.cs
@@ -8,9 +8,32 @@ public class SoundTransfrom : MonoBehaviour
     void Start()
     {
         Debug.Log(Microphone.devices[0]);
-        AudioSource audioSource = GetComponent<AudioSource>();
-        audioSource.clip = Microphone.Start(Microphone.devices[0], false, 10, 44100);
+        //AudioSource audioSource = GetComponent<AudioSource>();
+        StartCoroutine(Example());
+        /*
+        audioSource.clip = Microphone.Start(Microphone.devices[0], true, 1, 44100);
         audioSource.Play();
+        Debug.Log(audioSource.clip.length);
+        float[] samples = new float[audioSource.clip.samples * audioSource.clip.channels];
+        audioSource.clip.GetData(samples, 0);
+        Debug.Log(samples.Length);
+        */
+    }
+    IEnumerator Example()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(Microphone.devices[0], true, 1, 44100);
+        audioSource.Play();
+        Debug.Log(audioSource.clip.length);
+
+        yield return new WaitForSeconds(2.5f);
+        float[] samples = new float[audioSource.clip.samples * audioSource.clip.channels];
+        audioSource.clip.GetData(samples, 0);
+        Debug.Log(samples.Length);
+        foreach (float weh in samples)
+        {
+            Debug.Log(weh);
+        }
     }
 
     // Update is called once per frame
