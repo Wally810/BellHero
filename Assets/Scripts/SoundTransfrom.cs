87c7c1899f8f5463b850361d5ba86d5399cebeed
Charley Dodgion <charleydodgion@suumail.net>
2024-02-08 11:10:26 -0700

    Recording Testing

--- Assets/Scripts/SoundTransfrom.cs
+++ Assets/Scripts/SoundTransfrom.cs
@@ -7,10 +7,10 @@ public class SoundTransfrom : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        foreach( var weh in Microphone.devices)
-        {
-            Debug.Log(weh);
-        }   
+        Debug.Log(Microphone.devices[0]);
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(Microphone.devices[0], false, 10, 44100);
+        audioSource.Play();
     }
 
     // Update is called once per frame
