a4b2ed1c9bc8101e03978c3692656040e2434de8
Charley Dodgion <charleydodgion@suumail.net>
2024-02-08 11:01:32 -0700

    Finding Microphones

--- Assets/Scripts/SoundTransfrom.cs
+++ Assets/Scripts/SoundTransfrom.cs
@@ -7,7 +7,10 @@ public class SoundTransfrom : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach( var weh in Microphone.devices)
+        {
+            Debug.Log(weh);
+        }   
     }
 
     // Update is called once per frame
